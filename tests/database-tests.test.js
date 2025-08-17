c6cb0ece123db6e790cc4a985ea0d20c81d770e1
Float07 <thiago.d.c.de.pauli@gmail.com>
2025-08-17 17:28:56 -0300

    Create tests for database to exemplify use of beforeAll

--- tests/database-tests.test.js
+++ tests/database-tests.test.js
@@ -1,1 +1,27 @@
+const { test, expect, beforeAll } = require('@jest/globals');
+const { fakerPT_BR: faker } = require('@faker-js/faker');
+const { Database } = require('../lib/database');
 
+beforeAll(async () => {
+  // Crate people database and populate it
+  const peopleDatabase = new Database('people');
+  await peopleDatabase.untilReady;
+
+  const people = Array.from({length: 100000}, () => ({
+    firstName: faker.person.firstName(),
+    middleName: faker.person.middleName(),
+    lastName: faker.person.lastName(),
+    job: faker.person.jobTitle(),
+    bio: faker.person.bio(),
+  }))
+
+  peopleDatabase.documents = people;
+
+  await peopleDatabase.save();
+});
+
+test('database has 100000 documents', async () => {
+  const peopleDatabase = new Database('people');
+  await peopleDatabase.untilReady;
+  expect(peopleDatabase.documents.length).toBe(100000);
+})
